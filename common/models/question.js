9bc38046afd09818a5958fecf154c4860fdd9bbe
mf256 <44268440+mf256@users.noreply.github.com>
2018-11-25 15:06:15 +0100

    add custom function

--- common/models/question.js
+++ common/models/question.js
@@ -1,31 +1,30 @@
+/* eslint-disable comma-dangle */
 'use strict';
 
 module.exports = function(Question) {
+// Call remote Method
+  Question.voteById = function(id, cb) {
+  // code goes here
+    Question.findById(id, function(err, instance) {
+    // updateAll([where], data, callback)
+      var callParams = {votes: instance.votes + 1};
+      Question.updateAll({id: instance.id}, callParams, function(err, instance2) {
+        var out = Object.assign({}, instance.__data, callParams);
+        cb(null, out);
+      });
+    });
+  };
 
-    Question.vote = function(cb) {
-          var currentDate = new Date();
-          var currentHour = currentDate.getHours();
-          var OPEN_HOUR = 6;
-          var CLOSE_HOUR = 20;
-          console.log('Current hour is %d', currentHour);
-          var response;
-          if (currentHour >= OPEN_HOUR && currentHour < CLOSE_HOUR) {
-            response = 'We are open for business.';
-          } else {
-            response = 'Sorry, we are closed. Open daily from 6am to 8pm.';
-          }
-          cb(null, response);
-        };
-        Question.remoteMethod(
-          'vote', {
-            http: {
-              path: '/status',
-              verb: 'post'
-            },
-            returns: {
-              arg: 'status',
-              type: 'string'
-            }
-          }
-        );
+// Create remote Method
+  Question.remoteMethod('voteById', {
+    http: {path: '/:id/vote', verb: 'get'},
+    accepts: [
+      {arg: 'id', type: 'string', http: {source: 'path'},
+        required: true, 'description': 'Model id'
+      },
+    ],
+    returns: {root: true, type: 'object'},
+    description: 'Increment vote attribute for a model ' +
+      'instance and persist it into the data source.',
+  });
 };
